da131e95397bd5d5085b42acf6b292f0b59afa94
Said Mg <saidmghabghab@gmail.com>
2021-02-27 20:24:22 -0500

    routers in burgers_controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,34 @@
-const Express = require(Express)
-const burger = require(burger.js)
+var express = require('express');
+var router = express.Router()
+const orm = require("../config/orm");
+
+
+
+router.get('/', async function (req, res) {
+    const burgers = await orm.selectAll()
+
+    // return it within handlebars
+    res.render('index', { tasks: burgers });
+})
+
+router.get('/', async function (req, res) {
+    const result = await orm.updateOne()
+    console.log('[/ PUT] updateBurger:', updateBurger);
+    res.send(result)
+})
+
+router.post('/api/burger', async function (req, res) {
+    let burgerName = req.body
+    console.log(burgerName)
+    const insertBurger = await orm.insertOne(burgerName)
+    console.log('[/ POST] insertBurger:', insertBurger);
+})
+
+router.post('/api/burger/:id', async function (req, res) {
+    const id = req.params.id
+    console.log(id)
+    const updateBurger = await orm.updateOne(id)
+    console.log('[/ PUT] updateBurger:', updateBurger);
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express')  
-
 const exphbs = require('express-handlebars');
-const orm = require('./config/orm');
 
 const app = express()
 const PORT = process.env.PORT || 8080;
@@ -12,44 +10,11 @@ app.use(express.json());
 
 // for HANDLEBARS paths
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.use(express.static(__dirname));
 app.set('view engine', 'handlebars');
 
-
-app.get('/', async function(req, res){
-    const burgers = await orm.selectAll()
-    // console.log('[/ GET] burgers:', burgers);
-
-    // return it within handlebars
-    res.render('index', { tasks: burgers });
-})
-
-app.get('/', async function(req, res){
-    const result = await orm.updateOne()
-    console.log('[/ PUT] updateBurger:', updateBurger);
-    res.send(result)
-})
-
-app.post('/api/burger', async function(req, res){
-    let burgerName = req.body 
-    console.log(burgerName)
-    const insertBurger = await orm.insertOne(burgerName)
-    console.log('[/ POST] insertBurger:', insertBurger);
-})
-
-app.post('/api/burger/:id', async function(req, res){
-    const id =  req.params.id 
-    console.log(id)
-    const updateBurger = await orm.updateOne(id)
-    console.log('[/ PUT] updateBurger:', updateBurger);
-})
-// app.post('/', async function(req, res){
-//   console.log( `[/ POST] Data received:`, req.body )
-
-  
-//   const result = await db.query( 'INSERT INTO tasks (task) VALUES (?)', [req.body.task] )
-//   // trigger page reload
-//   res.redirect('/');
-// })
+var router = require('./controllers/burgers_controller')
+app.use('/', router)
 
 app.listen(PORT, () =>
   console.log(`Server listening on: http://localhost:${PORT}`)
